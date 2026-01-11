663cf26c54d28163479fdbb51722fe8ec2977e69
Jada Blankenship <jada4569@yahoo.com>
2026-01-10 22:46:00 -0600

    Navbar edits

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Afacad, Raleway, Inter } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.css'
 import "./globals.css";
 
@@ -10,6 +10,21 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const afacad = Afacad({
+  variable: "--font-afacad",
+  subsets: ["latin"],
+});
+
+const raleway = Raleway({
+  variable: "--font-raleway",
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,37 +3,36 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <body>
-        {/* Navigation Bar */}
-        <nav className="navbar navbar-expand-lg bg-body-tertiary" id="navbar">
-          <div className="container-fluid">
-            <a>
-              <Image src="logo.svg" alt="Logo" width={75} height={75} />
-            </a>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav mx-auto">
-                    <li className="nav-item">
-                      <a className="nav-link" >Home</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" >Readers</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" >About</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" >Contact</a>
-                    </li>
-                  </ul>
-              </div>
-          </div>
-        </nav>
+      {/* Navigation Bar */}
+      <nav className="navbar navbar-expand-lg bg-body-tertiary" id="navbar">
+        <div className="container-fluid">
+          <a className="name-home">
+            <Image src="logo.svg" alt="Logo" width={50} height={50} />
+            NederLees
+          </a>
+          <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav mx-auto">
+                  <li className="nav-item">
+                    <a className="nav-link" >Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" >Readers</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" >About</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" >Contact</a>
+                  </li>
+                </ul>
+            </div>
+        </div>
+      </nav>
 
-        {/* Main Body of Page */}
-        <main>
+      {/* Main Body of Page */}
+      <main>
 
-        </main>
-      </body>
+      </main>
     </div>
   );
 }
