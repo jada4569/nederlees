82c8a27c9a0790f97701856e701524c02c2969dd
Jada Blankenship <jada4569@yahoo.com>
2026-01-18 16:10:56 -0600

    Add initial all readers page

--- app/readers/page.tsx
+++ app/readers/page.tsx
@@ -5,7 +5,34 @@ export default function Readers() {
     <div>
       {/* Main Body of Page */}
       <main>
-
+        <div className="container raleway-font off-white-bg py-5">
+          <div className="align-center">
+            <Image className="image-border-radius" src="/books.jpg" alt="Stack of books" width={350} height={350} />
+          </div>
+          <h2 className="bold-font orange-text align-center py-3">All Our Readers</h2>
+          <div className="row align-center">
+            <div className="col-3">
+              <button className="reader-button off-white-bg link-color bold-font">A0</button>
+            </div>
+            <div className="col-3">
+              <button className="reader-button off-white-bg link-color bold-font">A1</button>
+            </div>
+            <div className="col-3">
+              <button className="reader-button off-white-bg link-color bold-font">A2</button>
+            </div>
+            <div className="col-3">
+              <button className="reader-button off-white-bg link-color bold-font">B1</button>
+            </div>
+          </div>
+          <h4 className="dark-green-text bold-font pt-5">Level A0 Readers</h4>
+          <p>Story List Placeholder</p>
+          <h4 className="dark-green-text bold-font pt-5">Level A1 Readers</h4>
+          <p>Story List Placeholder</p>
+          <h4 className="dark-green-text bold-font pt-5">Level A2 Readers</h4>
+          <p>Story List Placeholder</p>
+          <h4 className="dark-green-text bold-font pt-5">Level B1 Readers</h4>
+          <p>Story List Placeholder</p>
+        </div>
       </main>
     </div>
   );
